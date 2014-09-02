a3e8871a04f206ccce9802992126f2d185494306
Javier Eguiluz <javier.eguiluz@gmail.com>
2014-09-02 12:28:56 +0200

    We now look for DX issues in tens of GitHub repositories

--- src/AppBundle/Command/LookForIssuesCommand.php
+++ src/AppBundle/Command/LookForIssuesCommand.php
@@ -2,14 +2,18 @@
 
 namespace AppBundle\Command;
 
+use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
 use AppBundle\Entity\Issue;
 
 class LookForIssuesCommand extends ContainerAwareCommand
 {
+    private $rootDir;
+
     protected function configure()
     {
         $this
@@ -20,19 +24,54 @@ class LookForIssuesCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $client = new \Github\Client(
-            new \Github\HttpClient\CachedHttpClient(array(
-                'cache_dir' => __DIR__.'/../../../app/cache/github'
-            ))
-        );
-
-        $results = $client
-            ->api('issue')
-            ->all('symfony', 'symfony', array('labels' => 'DX')
-        );
-
+        $this->rootDir = __DIR__.'/../../../';
+        $repositories = $this->getRepositoriesToScan();
         $em = $this->getContainer()->get('doctrine')->getManager();
 
+        $output->writeln("Looking for DX Issues");
+        $output->writeln("=====================\n");
+
+        foreach ($repositories as $repository) {
+            $output->writeln("> Scanning $repository");
+
+            $results = $this->findIssues($repository);
+            $this->saveIssues($em, $results);
+        }
+
+        $em->flush();
+    }
+
+    private function getRepositoriesToScan()
+    {
+        $repositories = Yaml::parse($this->rootDir.'/app/config/repositories.yml');
+
+        return $repositories['repositories'];
+    }
+
+    private function getGitHubClient()
+    {
+        return new \Github\Client(
+            new \Github\HttpClient\CachedHttpClient(array(
+                'cache_dir' => $this->rootDir.'/app/cache/github'
+            ))
+        );
+    }
+
+    private function findIssues($repositoryUrl)
+    {
+        list($vendor, $repository) = explode('/', $repositoryUrl);
+
+        return $this->getGitHubClient()
+            ->api('issue')
+            ->all($vendor, $repository, array(
+                'labels' => 'DX',
+                'state' => 'all', // by default it only looks for 'open' issues
+            )
+        );
+    }
+
+    private function saveIssues(EntityManager $em, $results)
+    {
         foreach ($results as $result) {
             $entity = $em->getRepository('AppBundle:Issue')->findOneBy(array(
                 'title' => $result['title']
@@ -52,8 +91,6 @@ class LookForIssuesCommand extends ContainerAwareCommand
                 $em->persist($issue);
             }
         }
-
-        $em->flush();
     }
 
     private function getIssueStatus(array $issue)
